b840cdb5823227de89357932987afec3370888db
programmerinprogress <msharp23@hotmail.co.uk>
2015-08-13 18:49:45 +0100

    And now the contact form works in production!

--- server/api/mailer/mailer.controller.js
+++ server/api/mailer/mailer.controller.js
@@ -9,7 +9,6 @@ exports.index = function(req, res) {
 };
 
 exports.create = function(req, res) {
-  debugger;
   var transporter = nodemailer.createTransport({
     service: 'Hotmail',
     auth: {
@@ -23,14 +22,14 @@ var mailOptions = {
     from:  process.env.EMAIL_LOGIN, // sender address
     to:  process.env.EMAIL_LOGIN, // list of receivers
     subject: 'A user query from michaeldsharp.com', // Subject line
-    text: 'message from: '+ req.body.email + (req.body.name != "" ? '(' + req.body.name + ')' : '')  + '\n\n' + req.body.message // plaintext body
+    text: 'message from: '+ req.body.email + (req.body.name !== "" ? '(' + req.body.name + ')' : '')  + '\n\n' + req.body.message // plaintext body
 };
 
 transporter.sendMail(mailOptions, function(error, info){
     if(error){
         return console.log(error);
     }
-    
+
     res.json({message : "Message sent!"});
 });
 
