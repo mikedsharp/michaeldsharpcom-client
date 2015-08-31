808c09b54514dbd95b7c760611d63bf87ece0d0b
programmerinprogress <msharp23@hotmail.co.uk>
2015-08-31 10:14:46 +0200

    generated structure for project list API, mocked up structure I'm going to use. WIP

--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,7 @@ var path = require('path');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/projects', require('./api/project'));
   app.use('/api/mailers', require('./api/mailer'));
   app.use('/api/things', require('./api/thing'));
 
