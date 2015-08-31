8ce5e94ebebbd3e890a8afb1070c32b38de4c82a
programmerinprogress <msharp23@hotmail.co.uk>
2015-08-31 11:05:49 +0200

    Added plumbing between Angular and project API, we now have a data source!

--- client/app/projects/projects.controller.js
+++ client/app/projects/projects.controller.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('michaeldsharpcomApp')
-  .controller('ProjectsCtrl', function ($scope) {
+  .controller('ProjectsCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.message = 'Hello';
-  });
+    $scope.projects = [];
+
+    $http.get('/api/projects').
+    then(function(response) {
+      // this callback will be called asynchronously
+      // when the response is available
+      $scope.projects = response.data;
+    }, function(response) {
+      $scope.projects = [];
+      // called asynchronously if an error occurs
+      // or server returns response with an error ∫status.
+    });
+
+  }]);
--- server/api/project/project.controller.js
+++ server/api/project/project.controller.js
@@ -1,33 +1,12 @@
 'use strict';
 
 var _ = require('lodash');
+var fs = require('fs');
+
 
 // Get list of projects
 exports.index = function(req, res) {
-  res.json([{
-    title : "Rebound",
-    subtitle : "A game I made for a competition",
-    description: "look at this amazing thing I made, hahahahaha, give me money!",
-    images : {
-      mainImage : {
-        title : "the main image",
-        url : "imageurl-main.jpg"
-      },
-      gallery: [
-        {
-          title: "an image",
-          imageThumbUrl : "thing-thumb.jpg",
-          imageLargeUrl : "thing-large.jpg"
-        }
-      ],
-      links : [{
-        title : "link A",
-        url: "linka.html"
-      }],
-      videos : [{
-        title : "video A",
-        url : "youtube.com/embed?thing=5"
-      }]
-    }
-  }]);
+  var data = JSON.parse(fs.readFileSync( __dirname + '/projects.json', 'utf8'));
+
+  res.json(data);
 };
