49f2c55453f2b710bf51d41e6186be04817af067
Mike Sharp <msharp23@hotmail.co.uk>
2016-12-02 22:28:13 +0000

    Nuking the entire site, going to rebuld on develop

--- client/app/contact/contact.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('michaeldsharpcomApp')
-  .controller('ContactCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
--- client/components/footer/footer.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('michaeldsharpcomApp')
-  .controller('FooterCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
