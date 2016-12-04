7d00c9189566701be525d142c03d83d88506b628
Mike Sharp <msharp23@hotmail.co.uk>
2016-12-04 16:49:50 +0000

    refining the navigation styling, testing logos out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome'; 
+ 
 
 import { AppComponent } from './app.component';
 import { routing, routedComponents } from './app.routing';
@@ -18,7 +20,8 @@ import { NavComponent } from './shared/nav/nav.component';
     BrowserModule,
     FormsModule,
     HttpModule, 
-    routing
+    routing, 
+    Angular2FontawesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
