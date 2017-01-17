d37ed0c8b6185e3914f1f083d31432bc58febd0b
Mike Sharp <msharp23@hotmail.co.uk>
2017-01-17 07:13:02 +0000

    added conditional to remove loader frrom DOM when view initialised

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-
+import {OnInit} from '@angular/core'; 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'michaeldsharp.com, under development and coming soon!';
-  showingHamburger = false; 
+  showingHamburger = false;
+  loading = true;  
   
   public constructor(private titleService: Title){
     this.titleService.setTitle('Mike D Sharp - Portfolio'); 
     this.showingHamburger = false; 
 }
+  ngOnInit(){
+    this.loading = false; 
+  }
 
   toggleHamburger(){
     this.showingHamburger = !this.showingHamburger; 
