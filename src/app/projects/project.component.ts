2a25346f039168758d200a0555b0aae56aa1e15b
Mike Sharp <msharp23@hotmail.co.uk>
2020-04-12 01:48:19 +0100

    enabled hash routing (I think)

--- src/app/projects/project.component.ts
+++ src/app/projects/project.component.ts
@@ -1,34 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router'; 
-import {ProjectService} from './project.service'; 
-import {Observable} from 'rxjs/Observable'; 
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { ProjectService } from "./project.service";
+import { Observable } from "rxjs/Observable";
 
 @Component({
-    templateUrl: 'project.component.html', 
-    styleUrls: ['project.component.scss'], 
-    selector:'project-view'
+  templateUrl: "project.component.html",
+  styleUrls: ["project.component.scss"],
+  selector: "project-view",
 })
 export class ProjectViewComponent implements OnInit {
-    private paramSubscription: any; 
-    private id: string; 
-    private project: any = null; 
-    private baseScreenshotUrl:string; 
-    
-    constructor(private route: ActivatedRoute, private projects: ProjectService) { 
-        this.baseScreenshotUrl = 'https://s3-eu-west-1.amazonaws.com/michaeldsharp.com/assets/thumbs'; 
-    }
+  public paramSubscription: any;
+  public id: string;
+  public project: any = null;
+  public baseScreenshotUrl: string;
 
-    openLink(link){
-        window.open(link.url, '_blank'); 
-    }
+  constructor(private route: ActivatedRoute, private projects: ProjectService) {
+    this.baseScreenshotUrl =
+      "https://s3-eu-west-1.amazonaws.com/michaeldsharp.com/assets/thumbs";
+  }
 
-    ngOnInit() { 
-        this.paramSubscription = this.route.params.subscribe(params => {
-            let id  = params['id']; 
-            
-            this.projects.getProject(id)
-                         .subscribe(data =>  this.project = data);
-        }); 
+  openLink(link) {
+    window.open(link.url, "_blank");
+  }
 
-    }
+  ngOnInit() {
+    this.paramSubscription = this.route.params.subscribe((params) => {
+      let id = params["id"];
+
+      this.projects.getProject(id).subscribe((data) => (this.project = data));
+    });
+  }
 }
