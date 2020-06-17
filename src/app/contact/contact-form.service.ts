61548bac07c8c3c5b42730d0434c3491ba20a5b9
Mike Sharp <msharp23@hotmail.co.uk>
2020-06-17 16:59:38 +0100

    switched to using a custom domain name for api/.

--- src/app/contact/contact-form.service.ts
+++ src/app/contact/contact-form.service.ts
@@ -1,19 +1,17 @@
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import {ContactFormData} from './contact-form-data';
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import { ContactFormData } from "./contact-form-data";
 
 @Injectable()
 export class ContactFormService {
+  domain = "https://api.michaeldsharp.com";
+  endpoint = "/contact";
 
-    domain = 'https://p24zg9jpwh.execute-api.eu-west-1.amazonaws.com';
-    endpoint = '/default/contact';
+  constructor(private http: Http) {}
 
-    constructor(private http: Http) {
-    }
-
-    submitForm(formData: ContactFormData) {
-        return this.http
-            .post(this.domain + this.endpoint, formData)
-            .map((res: any) => res.json());
-    }
+  submitForm(formData: ContactFormData) {
+    return this.http
+      .post(this.domain + this.endpoint, formData)
+      .map((res: any) => res.json());
+  }
 }
